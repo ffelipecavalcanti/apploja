282a820b1827281d9eaad426a4483a5a579425f4
Felipe Cavalcanti <felipecavalcanti033@gmail.com>
2019-11-06 12:37:09 -0300

    Projeto criado

---
+++ src/main/java/com/applojamc/ApplojaApplication.java
@@ -0,0 +1,12 @@
+package com.applojamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplojaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/applojamc/apploja/ApplojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applojamc.apploja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
