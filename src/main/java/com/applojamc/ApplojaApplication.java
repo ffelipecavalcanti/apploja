cf8a613dab464cb3fe181144c86022603e6e1875
Felipe Cavalcanti <felipecavalcanti033@gmail.com>
2019-11-07 18:13:27 -0300

    Estado e cidade

--- src/main/java/com/applojamc/ApplojaApplication.java
+++ src/main/java/com/applojamc/ApplojaApplication.java
@@ -8,9 +8,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.applojamc.domain.Categoria;
+import com.applojamc.domain.Cidade;
+import com.applojamc.domain.Estado;
 import com.applojamc.domain.Produto;
 import com.applojamc.repositories.CategoriaRepository;
+import com.applojamc.repositories.CidadeRepository;
+import com.applojamc.repositories.EstadoRepository;
 import com.applojamc.repositories.ProdutoRepository;
+
+import javassist.expr.NewArray;
 
 @SpringBootApplication
 public class ApplojaApplication implements CommandLineRunner {
@@ -20,6 +26,12 @@ public class ApplojaApplication implements CommandLineRunner {
 	
 	@Autowired
 	ProdutoRepository produtoRepository; 
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplojaApplication.class, args);
@@ -44,5 +56,18 @@ public class ApplojaApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3)); 
+		
+		Estado est1 = new Estado(null, "Minas Gerais"); 
+		Estado est2 = new Estado(null, "São Paulo"); 
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1); 
+		Cidade c2 = new Cidade(null, "São Paulo", est2); 
+		Cidade c3 = new Cidade(null, "Campinas", est2); 
+
+		est1.getCidades().addAll(Arrays.asList(c1)); 
+		est2.getCidades().addAll(Arrays.asList(c2, c3)); 
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2)); 
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3)); 
 	}
 }
