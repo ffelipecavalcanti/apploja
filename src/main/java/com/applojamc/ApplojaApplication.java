aa3330f38fdc4003ab4e9d2126f3636929dea467
Felipe Cavalcanti <felipecavalcanti033@gmail.com>
2019-11-07 12:51:29 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/applojamc/ApplojaApplication.java
+++ src/main/java/com/applojamc/ApplojaApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.applojamc.domain.Categoria;
+import com.applojamc.domain.Produto;
 import com.applojamc.repositories.CategoriaRepository;
+import com.applojamc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ApplojaApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository categoriaRepository; 
+	
+	@Autowired
+	ProdutoRepository produtoRepository; 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplojaApplication.class, args);
@@ -26,6 +31,18 @@ public class ApplojaApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório"); 
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00); 
+		Produto p2 = new Produto(null, "Impressora", 800.00); 
+		Produto p3 = new Produto(null, "Mouse", 80.00); 
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3)); 
+		cat2.getProdutos().addAll(Arrays.asList(p2)); 
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1)); 
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2)); 
+		p3.getCategorias().addAll(Arrays.asList(cat1)); 
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3)); 
 	}
 }
