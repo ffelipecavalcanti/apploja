76416e5a54336267a8a0579bd373c38243b6c427
Felipe Cavalcanti <felipecavalcanti033@gmail.com>
2019-11-08 14:06:08 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/applojamc/ApplojaApplication.java
+++ src/main/java/com/applojamc/ApplojaApplication.java
@@ -1,5 +1,6 @@
 package com.applojamc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.applojamc.domain.Cidade;
 import com.applojamc.domain.Cliente;
 import com.applojamc.domain.Endereco;
 import com.applojamc.domain.Estado;
+import com.applojamc.domain.Pagamento;
+import com.applojamc.domain.PagamentoComBoleto;
+import com.applojamc.domain.PagamentoComCartao;
+import com.applojamc.domain.Pedido;
 import com.applojamc.domain.Produto;
+import com.applojamc.domain.enums.EstadoPagamento;
 import com.applojamc.domain.enums.TipoCliente;
 import com.applojamc.repositories.CategoriaRepository;
 import com.applojamc.repositories.CidadeRepository;
 import com.applojamc.repositories.ClienteRepository;
 import com.applojamc.repositories.EnderecoRepository;
 import com.applojamc.repositories.EstadoRepository;
+import com.applojamc.repositories.PagamentoRepository;
+import com.applojamc.repositories.PedidoRepository;
 import com.applojamc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class ApplojaApplication implements CommandLineRunner {
 	
 	@Autowired
 	EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	PedidoRepository pedidoRepository;
+	
+	@Autowired
+	PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplojaApplication.class, args);
@@ -89,5 +103,21 @@ public class ApplojaApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2)); 
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, null, sdf.parse("20/10/2017 00:00"));
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 }
---
+++ src/main/java/com/applojamc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.applojamc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.applojamc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
