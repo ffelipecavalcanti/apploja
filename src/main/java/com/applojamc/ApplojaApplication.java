2f24e0bae9f21a18b7972d6729679eb41051c114
Felipe Cavalcanti <felipecavalcanti033@gmail.com>
2019-11-08 11:45:11 -0300

    Cliente, TipoCliente, telefones e endereços

--- src/main/java/com/applojamc/ApplojaApplication.java
+++ src/main/java/com/applojamc/ApplojaApplication.java
@@ -9,14 +9,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.applojamc.domain.Categoria;
 import com.applojamc.domain.Cidade;
+import com.applojamc.domain.Cliente;
+import com.applojamc.domain.Endereco;
 import com.applojamc.domain.Estado;
 import com.applojamc.domain.Produto;
+import com.applojamc.domain.enums.TipoCliente;
 import com.applojamc.repositories.CategoriaRepository;
 import com.applojamc.repositories.CidadeRepository;
+import com.applojamc.repositories.ClienteRepository;
+import com.applojamc.repositories.EnderecoRepository;
 import com.applojamc.repositories.EstadoRepository;
 import com.applojamc.repositories.ProdutoRepository;
-
-import javassist.expr.NewArray;
 
 @SpringBootApplication
 public class ApplojaApplication implements CommandLineRunner {
@@ -32,6 +35,12 @@ public class ApplojaApplication implements CommandLineRunner {
 	
 	@Autowired
 	CidadeRepository cidadeRepository;
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@Autowired
+	EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplojaApplication.class, args);
@@ -69,5 +78,16 @@ public class ApplojaApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2)); 
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3)); 
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2)); 
 	}
 }
---
+++ src/main/java/com/applojamc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.applojamc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.applojamc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
