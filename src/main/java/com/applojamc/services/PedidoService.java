7175f6271bc8a4a651ed27b5221a702134802af2
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2019-11-19 13:44:21 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/applojamc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.applojamc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.applojamc.domain.Pedido;
+import com.applojamc.repositories.PedidoRepository;
+import com.applojamc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
