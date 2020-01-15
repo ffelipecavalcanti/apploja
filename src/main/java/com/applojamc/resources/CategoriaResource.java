bb04e34005c228ab1e5b37b376224733c423a169
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-01-15 13:33:06 -0300

    Listando todas categorias com DTO

--- src/main/java/com/applojamc/resources/CategoriaResource.java
+++ src/main/java/com/applojamc/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.applojamc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.applojamc.domain.Categoria;
+import com.applojamc.dto.CategoriaDTO;
 import com.applojamc.services.CategoriaService;
 
 @RestController
@@ -46,5 +50,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		serv.delete(id); 
 		return ResponseEntity.noContent().build(); 
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = serv.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()); 
+		return ResponseEntity.ok(listDto);
 	}
 }
--- src/main/java/com/applojamc/services/CategoriaService.java
+++ src/main/java/com/applojamc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.applojamc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui produto(s)"); 
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll(); 
 	}
 }
