2d54343ca2f40457719e4037eb6aa1cd1d6a3cb2
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-01-17 08:41:28 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/applojamc/resources/CategoriaResource.java
+++ src/main/java/com/applojamc/resources/CategoriaResource.java
@@ -6,11 +6,13 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -57,6 +59,19 @@ public class CategoriaResource {
 		
 		List<Categoria> list = serv.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()); 
+		return ResponseEntity.ok(listDto);
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+		
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, // 24 é multiplo de 1, 2, 3, 4 entao fica facil de organizar o layout de forma responsiva. 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		
+		Page<Categoria> list = serv.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj)); // A classe Page é java-8 compliance e não precisa do stream e collect.
 		return ResponseEntity.ok(listDto);
 	}
 }
