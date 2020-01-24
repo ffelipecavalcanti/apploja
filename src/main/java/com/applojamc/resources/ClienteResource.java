8c891adc49e805779e3cb341f29dbf340ebe8f7b
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-01-24 12:40:03 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/applojamc/resources/ClienteResource.java
+++ src/main/java/com/applojamc/resources/ClienteResource.java
@@ -1,26 +1,70 @@
 package com.applojamc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.applojamc.domain.Cliente;
+import com.applojamc.dto.ClienteDTO;
 import com.applojamc.services.ClienteService;
 
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
-	
+
 	@Autowired
-	private ClienteService serv; 
-	
-	@RequestMapping(value = "/{id}" ,method = RequestMethod.GET)
+	private ClienteService serv;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-		
+
 		Cliente obj = serv.find(id);
 		return ResponseEntity.ok(obj);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable Integer id, @Valid @RequestBody ClienteDTO objDto) {
+		Cliente obj = serv.fromDTO(objDto);
+		obj.setId(id);
+		obj = serv.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		serv.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+
+		List<Cliente> list = serv.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, // 24 é multiplo de 1, 2, 3, 4 entao fica facil de organizar o layout de forma responsiva.
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		Page<Cliente> list = serv.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj)); // A classe Page é java-8 compliance e não precisa do stream e collect.
+		return ResponseEntity.ok(listDto);
 	}
 }
