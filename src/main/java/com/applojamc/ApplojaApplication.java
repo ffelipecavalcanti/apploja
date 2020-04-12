cf5735e6de1c53fd8e27ab9099bcf40754d460dd
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-04-12 19:20:36 -0300

    Salvando arquivo no S3 da Amazon

--- src/main/java/com/applojamc/ApplojaApplication.java
+++ src/main/java/com/applojamc/ApplojaApplication.java
@@ -1,17 +1,25 @@
 package com.applojamc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.applojamc.services.S3Service;
+
 @SpringBootApplication
 public class ApplojaApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplojaApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {	
+	public void run(String... args) throws Exception {
+		
+		s3Service.uploadFile("C:\\temp\\fotos\\foto-luiz.jpg");
 	}
 }
