6bcba02b42240eba09afec894f51f81a1e973c2b
Luiz Cornelli <luiz.cavalcanti@unidac.com.br>
2020-04-12 20:34:51 -0300

    Tratando exceções adequadamente

--- src/main/java/com/applojamc/services/S3Service.java
+++ src/main/java/com/applojamc/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.applojamc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -44,7 +45,7 @@ public class S3Service {
 			return uploadFile(is, fileName, contentType);
 
 		} catch (IOException e) {
-			throw new RuntimeException("Erro do IO: " + e.getMessage());
+			throw new FileException("Erro do IO: " + e.getMessage());
 		}
 	}
 	
@@ -64,7 +65,7 @@ public class S3Service {
 			return amazonS3.getUrl(bucketName, fileName).toURI();
 
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
